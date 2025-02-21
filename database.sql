7d567ce779a1dfdcf117069815cc23e8dfa4e4bc
Juan <jceulogio243@gmail.com>
2025-02-20 22:21:23 -0600

    created database - Juan Eulogio

---
+++ database.sql
@@ -0,0 +1,52 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.2 (Postgres.app)
+-- Dumped by pg_dump version 17.2 (Postgres.app)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: bills; Type: TABLE; Schema: public; Owner: juaneulogio
+--
+
+CREATE TABLE public.bills (
+    billnumber character varying(255),
+    billid integer,
+    billstatusdate date,
+    billstatus integer,
+    billtitle character varying(255),
+    billdescription character varying(255),
+    aisummary character varying(255)
+);
+
+
+ALTER TABLE public.bills OWNER TO juaneulogio;
+
+--
+-- Data for Name: bills; Type: TABLE DATA; Schema: public; Owner: juaneulogio
+--
+
+COPY public.bills (billnumber, billid, billstatusdate, billstatus, billtitle, billdescription, aisummary) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
